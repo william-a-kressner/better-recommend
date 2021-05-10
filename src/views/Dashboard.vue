aba05143f0b265d13a0f10af06c5b7af526c964a
William <william.kressner@gmail.com>
2021-05-10 16:48:08 -0400

    Get access token

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Welcome, {{ user.name }}</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: { 
+                name: ''
+            },
+            accessToken: ''
+        }
+    },
+    mounted() {
+        this.accessToken = window.location.toString().split("access_token=")[1].split("&")[0]
+        console.log(this.accessToken)
+    }
+}
+</script>
