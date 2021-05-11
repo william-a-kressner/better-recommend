a49381199749fc7b90d2a520c64b9c768df443cd
William <william.kressner@gmail.com>
2021-05-11 13:11:39 -0400

    Move the router push

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>You are listening to {{ currentSong.name }} by {{ currentSong.artist }} off the album {{ currentSong.albumName }}</h1>
-    <img id="album-cover" :src="currentSong.albumArtUrl" alt="Album art">
+    <img id="album-cover" :src="currentSong.albumArtUrl" alt="Album art" crossorigin="anonymous">
 </template>
 
 <script>
@@ -20,36 +20,36 @@ export default {
     async mounted() {
         if (!localStorage.accessToken) {
             localStorage.accessToken = window.location.toString().split("access_token=")[1].split("&")[0]
-            this.$router.push('/dashboard')
         }
+        this.$router.push('/dashboard')
         this.getCurrentSong()
         setInterval(this.getCurrentSong, 5000)
-
     },
     methods: {
         async getCurrentSong(){
             const headers = new Headers()
-            headers.append('Authorization', `Bearer ${this.accessToken}`)
+            headers.append('Authorization', `Bearer ${localStorage.accessToken}`)
             const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
                 method: 'get',
                 headers,
             })
             const data = await response.json()
-            if (this.currentSong.name !== data.item.name) {
+            const oldUrl = this.currentSong?.albumArtUrl
+            if (this.currentSong?.name !== data.item.name) {
                 this.currentSong = {
                     name: data.item.name,
                     albumName: data.item.album.name,
                     albumArtUrl: data.item.album.images[0].url,
                     artist: data.item.artists[0].name
                 }
-                this.getColors()
+                if (oldUrl !== this.currentSong.albumArtUrl) this.getColors()
             }
         },
-        getColors() {
+        async getColors() {
             const colorThief = new ColorThief()
-            // const albumArtImg = document.getElementById('album-cover').getAttribute('src')
-            console.log(this.currentSong.albumArtUrl)            
-            colorThief.getColor(this.currentSong.albumArtUrl).then(color => { console.log(color)}).then(err => { console.log(err)})            
+            const albumArtImg = document.getElementById('album-cover')
+            const colorArr = await colorThief.getPalette(albumArtImg)    
+            console.log(colorArr)
         }
     }
 }
